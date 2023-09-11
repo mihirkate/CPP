9a496613f1cf4879615a754b39e189ce1144e5fc
Mihir Kate <mihirkate13@gmail.com>
2023-09-12 01:21:28 +0530

    deleted spaces and unnecessary data

--- callbyvaluee.cpp
+++ callbyvaluee.cpp
@@ -1,35 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-void changevalue(int *address) 
+void changevalue(int *address)
 {
- *address =345;
-
+    *address = 345;
 }
+
 int main()
-   
 {
-int a=34,b=74;
-cout<<"the value of a now is"<<a<<endl;
-changevalue(&a);
-cout<<"the value of a now is"<<a<<endl;
-
-    return 0;
-}
-int main(int argc, char const *argv[])
-{
-    
-    
-
-
-
-    
-    return 0;
-}
-#include<iostream>
-using namespace std;
-int main(){
-    
+    int a = 34, b = 74;
+    cout << "the value of a now is" << a << endl;
+    changevalue(&a);
+    cout << "the value of a now is" << a << endl;
     return 0;
 }
